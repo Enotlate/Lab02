dab3ddca02480f1ec651c0f419d5e06b4cf0156c
Enotlate <106143455+Enotlate@users.noreply.github.com>
2023-10-14 01:04:29 +0300

    Comments in hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
 # include <iostream>
+//Супер пупупер позновательный комментарий
 int main(){
 	std::string dog_name;
+	// Вводим имя собаки
 	std:cout << "Enter your dog name: ";
 	std:cin >> dog_name;
 	std:cout << "Hello world from @" << dog_name << std::endl;
