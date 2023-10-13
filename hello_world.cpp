b32bad52647695a67003ca25016d681879abc381
Enotlate <bigalex03@mail.ru>
2023-10-14 00:33:44 +0300

    Add enter dog_name in  hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 # include <iostream>
 using namespace std;
 int main(){
-	cout << "Hello world!" << endl;
+	string dog_name;
+	cout << "Enter your dog name: ";
+	cin >> dog_name;
+	cout << "Hello world from @" << dog_name << endl;
 	return 0;
 }
