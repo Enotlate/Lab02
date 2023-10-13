de3c7acc5969c82d9b899afd38e47725a7d9ab67
Enotlate <bigalex03@mail.ru>
2023-10-14 00:58:09 +0300

    Rebase branch

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,15 @@
 # include <iostream>
 //Супер пупупер позновательный комментарий
-int main(){
-	std::string dog_name;
-	// Вводим имя собаки
-	std:cout << "Enter your dog name: ";
-	std:cin >> dog_name;
-	std:cout << "Hello world from @" << dog_name << std::endl;
-	return 0;
+int
+main()
+{
+  std::string dog_name;
+// Вводим имя собаки
+std:
+  cout << "Enter your dog name: ";
+std:
+  cin >> dog_name;
+std:
+  cout << "Hello world from @" << dog_name << std::endl;
+  return 0;
 }
